8986bf50209ca2e98b087af5b8b1888915d305ce
MatheusAmaralDeSousa <theusamarausyt@gmail.com>
2024-04-25 18:08:31 -0300

    Refactor: colors and structure

--- lib/navBar.dart
+++ lib/navBar.dart
@@ -1,55 +1,59 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: MyApp(),
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+
+  final List<IconData> _icons = [
+    Icons.home,
+    Icons.layers_outlined,
+    Icons.chrome_reader_mode,
+    Icons.settings,
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(child: Text('Fala Galera')),
+      body: Center(
+        child: Text('Pagina ${_selectedIndex + 1}'),
+      ),
       extendBody: true,
       bottomNavigationBar: BottomAppBar(
         shape: const CircularNotchedRectangle(),
-        color: Theme.of(context).colorScheme.primary,
+        color: Colors.black,
         child: IconTheme(
-          data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
+          data: IconThemeData(color: Colors.white),
           child: Padding(
             padding: const EdgeInsets.all(12.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.home,
+              children: _icons.asMap().entries.map((entry) {
+                final index = entry.key;
+                final icon = entry.value;
+                return IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _selectedIndex = index;
+                    });
+                  },
+                  icon: Icon(
+                    icon,
+                    color: _selectedIndex == index ? Colors.orange : Colors.white,
                   ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.layers_outlined,
-                  ),
-                ),
-                const SizedBox(width: 24),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.chrome_reader_mode,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.settings,
-                  ),
-                )
-              ],
+                );
+              }).toList(),
             ),
           ),
         ),
