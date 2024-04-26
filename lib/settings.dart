4be0a56c3386314df3a46efbaf158d494faee9e7
Mayhm <mayhm.social@proton.me>
2024-04-26 20:24:38 -0300

    feat: create open file explorer function

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,3 +1,8 @@
+import 'dart:io';
+
+import 'package:file_picker/_internal/file_picker_web.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatefulWidget {
@@ -8,6 +13,37 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  void _openFileExplorer(BuildContext context) async {
+    try {
+      FilePickerResult? result;
+      if (kIsWeb) {
+        result = await FilePickerWeb.platform.pickFiles(
+            type: FileType.custom,
+            allowedExtensions: ['json'],
+            dialogTitle: "Selecione seu backup");
+        if (result != null && result.files.isNotEmpty) {
+          final fileBytes = result.files.first.bytes;
+          final fileName = result.files.first.name;
+
+          print("FileName: $fileName");
+        }
+      } else {
+        result = await FilePicker.platform.pickFiles(
+            type: FileType.custom,
+            allowedExtensions: ['json'],
+            dialogTitle: "Selecione seu backup");
+        if (result != null && result.files.isNotEmpty) {
+          File file = File(result.files.single.path!);
+          print("File: $file");
+        } else {
+          print("Canceled the picker");
+        }
+      }
+    } catch (e) {
+      print("Error while picking the file: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,18 +51,16 @@ class _SettingsPageState extends State<SettingsPage> {
       body: ListView(
         children: [
           ListTile(
-            title: const Text('Exportar dados'),
-            subtitle: const Text(
-                'Exporte suas configurações através do arquivo JSON'),
+            title: const Text('Exportar backup'),
+            subtitle: const Text('Exporte seus dados através do arquivo JSON'),
             leading: const Icon(Icons.backup_rounded),
             onTap: () {},
           ),
           ListTile(
-            title: const Text('Importar dados'),
-            subtitle: const Text(
-                'Importe suas configurações através do arquivo JSON'),
+            title: const Text('Importar backup'),
+            subtitle: const Text('Importe seus dados através do arquivo JSON'),
             leading: const Icon(Icons.restore_rounded),
-            onTap: () {},
+            onTap: () => _openFileExplorer(context),
           ),
         ],
       ),
